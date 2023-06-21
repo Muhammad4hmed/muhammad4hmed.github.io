55ee7b736a7d6d18b08eb9f02c722d766a8283cb
Muhammad Ahmed <m.ahmed.memonn@gmail.com>
2023-06-21 16:43:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Add smooth scrolling to anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -7,4 +7,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       behavior: 'smooth'
     });
   });
+});
+
+// Move the navigation section along with the cursor
+const navList = document.getElementById('nav-list');
+document.addEventListener('mousemove', function (e) {
+  const mouseX = e.clientX;
+  const navListRect = navList.getBoundingClientRect();
+  const navItemWidth = navListRect.width / navList.children.length;
+
+  const activeIndex = Math.floor(mouseX / navItemWidth);
+
+  const activeLink = navList.querySelector('.active');
+  if (activeLink) {
+    activeLink.classList.remove('active');
+  }
+
+  const activeNavItem = navList.children[activeIndex].querySelector('a');
+  activeNavItem.classList.add('active');
+
+  const navSection = document.getElementById(activeNavItem.getAttribute('href').substring(1));
+  const navSectionRect = navSection.getBoundingClientRect();
+
+  const navSectionLeft = navSectionRect.left - navListRect.left;
+  const navSectionWidth = navSectionRect.width;
+  const navSectionCenter = navSectionLeft + navSectionWidth / 2;
+
+  navList.style.setProperty('--nav-section-left', `${navSectionLeft}px`);
+  navList.style.setProperty('--nav-section-width', `${navSectionWidth}px`);
+  navList.style.setProperty('--nav-section-center', `${navSectionCenter}px`);
 });
